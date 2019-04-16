26727661e042d9228662c2c1f906be433cfb31d5
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-16 15:13:41 -0700

    push

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitcd18da2edb26aadba05f70470b7b3a92::getLoader();
+return ComposerAutoloaderInit17b50a9907ec96fae7d514ce1eb38207::getLoader();
