5452656a5cc45501b78a990f801d047a333d9e79
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-17 10:17:33 -0700

    push

--- wp-content/themes/danko/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/danko/page-templates/homepage_template_parts/section-1.php
@@ -1,7 +1,21 @@
 <section id="section_one">
 	
+		<div class="sec_one_left">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/hero.jpg"/>
+			
+		</div><!-- sec_one_left -->
 		
+		
+		<div class="sec_one_right">
+			
+			<div class="sec_one_inner">
+				
+				<div class="play_button_wrapper"></div><!-- play_button_wrapper -->
+				
+			</div><!-- sec_one_inner -->
+			
+		</div><!-- sec_one_right -->
 
-	
 </section><!-- section_one -->
 
