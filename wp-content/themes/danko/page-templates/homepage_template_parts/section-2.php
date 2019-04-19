19336477a2b31bb4dac4100d8c2b1a6825459601
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-19 15:08:32 -0700

    push

---
+++ wp-content/themes/danko/page-templates/homepage_template_parts/section-2.php
@@ -0,0 +1,110 @@
+<section id="section_two">
+	
+	<div class="sec_two_inner">
+		
+		<div class="sec_two_slider">
+			
+			<div class="sec_two_single_slide">
+				
+				<div class="sec_two_reg">
+					
+					<div class="sec_two_reg_inner">
+						
+						<div class="img_wrapper">
+						
+							<img class="one" src="<?php bloginfo('template_directory');?>/images/result-01.svg"/>
+						
+						</div><!-- img_wrapper -->
+						
+						<span class="sec_two_amount">$50m</span><!-- sec_two_amount -->
+						
+						<span class="sec_two_type">Wrongful Death</span><!-- sec_two_type -->
+						
+					</div><!-- sec_two_reg_inner -->
+				
+				<div class="sec_two_hover">hover</div><!-- sec_two_hover -->
+				
+				</div><!-- sec_two_reg -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+			<div class="sec_two_single_slide">
+				
+				<div class="sec_two_reg">
+					
+					<div class="sec_two_reg_inner">
+						
+						<div class="img_wrapper">
+						
+							<img class="two" src="<?php bloginfo('template_directory');?>/images/result-02.svg"/>
+						
+						</div><!-- img_wrapper -->
+						
+						<span class="sec_two_amount">$50m</span><!-- sec_two_amount -->
+						
+						<span class="sec_two_type">Wrongful Death</span><!-- sec_two_type -->
+						
+					</div><!-- sec_two_reg_inner -->
+				
+				<div class="sec_two_hover">hover</div><!-- sec_two_hover -->
+				
+				</div><!-- sec_two_reg -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+			<div class="sec_two_single_slide">
+				
+				<div class="sec_two_reg">
+					
+					<div class="sec_two_reg_inner">
+						
+						<div class="img_wrapper">
+						
+							<img class="three" src="<?php bloginfo('template_directory');?>/images/result-03.svg"/>
+						
+						</div><!-- img_wrapper -->
+						
+						<span class="sec_two_amount">$14.9m</span><!-- sec_two_amount -->
+						
+						<span class="sec_two_type">Airplane Accident</span><!-- sec_two_type -->
+						
+					</div><!-- sec_two_reg_inner -->
+				
+				<div class="sec_two_hover">hover</div><!-- sec_two_hover -->
+				
+				</div><!-- sec_two_reg -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+			<div class="sec_two_single_slide">
+				
+				<div class="sec_two_reg">
+					
+					<div class="sec_two_reg_inner">
+						
+						<div class="img_wrapper">
+						
+							<img class="four" src="<?php bloginfo('template_directory');?>/images/result-04.svg"/>
+						
+						</div><!-- img_wrapper -->
+						
+						<span class="sec_two_amount">$9.5m</span><!-- sec_two_amount -->
+						
+						<span class="sec_two_type">Pedestrian Accident</span><!-- sec_two_type -->
+						
+					</div><!-- sec_two_reg_inner -->
+				
+				<div class="sec_two_hover">hover</div><!-- sec_two_hover -->
+				
+				</div><!-- sec_two_reg -->
+				
+			</div><!-- sec_two_single_slide -->
+			
+		
+			
+					
+		</div><!-- sec_two_slider -->
+		
+	</div><!-- sec_two_inner -->
+	
+</section><!-- section_two -->
