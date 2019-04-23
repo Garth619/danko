6f133a2a9c0775fcf2b16fbc6c8e0f52c8430a7c
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-23 10:55:45 -0700

    push

--- wp-content/themes/danko/footer.php
+++ wp-content/themes/danko/footer.php
@@ -1,6 +1,16 @@
-<footer id="footer_trigger">
+<footer id="footer_trigger" style="background:url(<?php bloginfo('template_directory');?>/images/pattern.jpg);background-size:78px 39px;">
 	
-	<?php // gravity_form(1, false, false, false, '', true, 12); ?>
+	<div class="footer_inner">
+		
+		<span class="footer_title">Free Consultation</span><!-- footer_title -->
+		
+		<span class="required"><span>*</span>-Required</span><!-- required -->
+		
+		<?php gravity_form(4, false, false, false, '', true, 12333); ?>
+		
+	</div><!-- footer_inner -->
+	
+	
 		
 </footer>
 
