9b53891aee2f79e0cdbb6bb688024d938acb4f9e
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-24 10:13:21 -0700

    push

--- wp-content/themes/danko/footer.php
+++ wp-content/themes/danko/footer.php
@@ -1,14 +1,19 @@
 <footer id="footer_trigger" style="background:url(<?php bloginfo('template_directory');?>/images/pattern.jpg);background-size:78px 39px;">
 	
-	<div class="footer_inner">
+	
+	<div class="footer_form_wrapper">
+	
+		<div class="footer_form_inner">
 		
-		<span class="footer_title">Free Consultation</span><!-- footer_title -->
+			<span class="footer_title">Free Consultation</span><!-- footer_title -->
 		
-		<span class="required"><span>*</span>-Required</span><!-- required -->
+			<span class="required"><span>*</span>-Required</span><!-- required -->
 		
-		<?php gravity_form(4, false, false, false, '', true, 12333); ?>
+			<?php gravity_form(4, false, false, false, '', true, 12333); ?>
 		
-	</div><!-- footer_inner -->
+	</div><!-- footer_form_inner -->
+	
+	</div><!-- footer_form_wrapper -->
 	
 	<div class="footer_location_wrapper">
 		
@@ -36,24 +41,28 @@
 			
 			<div class="footer_col_three footer_col">
 				
-				<ul>
-					<li><a class="" href="">Aviation Blog</a></li>
-					<li><a class="" href="">Personal Injury Blog</a></li>
-					<li><a class="" href="">Privacy Policy</a></li>
-					<li><a class="" href="">Disclaimer</a></li>
-				</ul>
+				<div class="footer_col_three_inner">
 				
-				<div class="copyright_wrapper">
+					<ul>
+						<li><a class="" href="">Aviation Blog</a></li>
+						<li><a class="" href="">Personal Injury Blog</a></li>
+						<li><a class="" href="">Privacy Policy</a></li>
+						<li><a class="" href="">Disclaimer</a></li>
+					</ul>
+				
+					<div class="copyright_wrapper">
 					
-					<span class="copyright">&copy;<?php echo date("Y"); ?> danko meredith. All rights reserved</span>
+						<span class="copyright">&copy;<?php echo date("Y"); ?> danko meredith. All rights reserved</span>
 					
-					<a class="" href="//ilawyermarketing.com" target="_blank" rel="noopener">
+						<a class="ilawyer" href="//ilawyermarketing.com" target="_blank" rel="noopener">
 						
-						<img src="<?php bloginfo('template_directory');?>/images/ilawyer.svg"/>
+							<img src="<?php bloginfo('template_directory');?>/images/ilawyer.svg"/>
 						
-					</a>
+						</a>
 					
-				</div><!-- copyright_wrapper -->
+					</div><!-- copyright_wrapper -->
+				
+				</div><!-- footer_col_three_inner -->
 				
 			</div><!-- footer_col_three -->
 			
