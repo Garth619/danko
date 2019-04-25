29f6f6131dd58fcdd7b435c3188ecb664d2cbc5d
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-25 10:10:21 -0700

    push

--- wp-content/themes/danko/page-templates/template-padirectory.php
+++ wp-content/themes/danko/page-templates/template-padirectory.php
@@ -4,9 +4,33 @@
 
 get_header(); ?>
 
-
+<div id="internal_main">
+	
+	<div class="inner_banner">
 		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		<div class="banner_content">
+			
+			<h1 class="banner_header"><?php the_title();?></h1>
+			
+			<a class="banner_button" href="#free_consultation">schedule free consultation</a>
+			
+		</div><!-- banner_content -->
+		
+		<img src="<?php bloginfo('template_directory');?>/images/hero-intl.jpg"/>
+		
+	</div><!-- inner_banner -->
+	
+	<div id="outer_trigger" class="outer_container">
+		
+		<div class="pa_directory_wrapper">
+			
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+				
+		</div><!-- pa_directory_wrapper -->
+		
+	</div><!-- outer_container -->
+	
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
