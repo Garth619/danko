00a88410e1b75f4ef3a5bcad39ad5839b6208daa
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-26 10:37:22 -0700

    push

--- wp-content/themes/danko/page-templates/template-testimonials.php
+++ wp-content/themes/danko/page-templates/template-testimonials.php
@@ -5,6 +5,102 @@
 get_header(); ?>
 
 
+<div id="internal_main">
+	
+	<div class="inner_banner">
+		
+		<div class="banner_content">
+			
+			<h1 class="banner_header"><?php the_title();?></h1>
+			
+			<a class="banner_button" href="#free_consultation">schedule free consultation</a>
+			
+		</div><!-- banner_content -->
+		
+		<img src="<?php bloginfo('template_directory');?>/images/hero-intl.jpg"/>
+		
+	</div><!-- inner_banner -->
+	
+	<div id="outer_trigger" class="outer_container">
+		
+		<div class="d_m_slide dm_internal">
+		
+			<span>D</span>
+			<span>M</span>
+	
+		</div><!-- d_m_slide -->
+		
+		<div class="testimonial_wrapper">
+			
+			<div class="single_testimonial">
+				
+				<div class="single_testimonial_inner">
+				
+					<img src="<?php bloginfo('template_directory');?>/images/ico-star-wreath.svg"/>
+				
+					<span class="testimonial_title">Massa tincidunt dui ut ornare. Faucibus interdum posuere lorem ipsum dolor sit.</span><!-- testimonial_title -->
+					
+					<p>Massa tincidunt dui ut ornare. Faucibus interdum posuere lorem ipsum dolor sit. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Hendrerit dolor magna eget est lorem ipsum dolor sit amet.</p>
+					
+					<div class="testimonial_meta">
+						
+						<span>Client Name</span>
+						<span>Type of Case</span>
+						
+					</div><!-- testimonial_meta -->
+				
+				</div><!-- single_testimonial_inner -->
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial">
+				
+				<div class="single_testimonial_inner">
+				
+					<img src="<?php bloginfo('template_directory');?>/images/ico-star-wreath.svg"/>
+				
+					<span class="testimonial_title">Massa tincidunt dui ut ornare. Faucibus interdum posuere lorem ipsum dolor sit.</span><!-- testimonial_title -->
+					
+					<p>Massa tincidunt dui ut ornare. Faucibus interdum posuere lorem ipsum dolor sit. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Hendrerit dolor magna eget est lorem ipsum dolor sit amet.</p>
+					
+					<div class="testimonial_meta">
+						
+						<span>Client Name</span>
+						<span>Type of Case</span>
+						
+					</div><!-- testimonial_meta -->
+				
+				</div><!-- single_testimonial_inner -->
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial">
+				
+				<div class="single_testimonial_inner">
+				
+					<img src="<?php bloginfo('template_directory');?>/images/ico-star-wreath.svg"/>
+				
+					<span class="testimonial_title">Massa tincidunt dui ut ornare. Faucibus interdum posuere lorem ipsum dolor sit.</span><!-- testimonial_title -->
+					
+					<p>Massa tincidunt dui ut ornare. Faucibus interdum posuere lorem ipsum dolor sit. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Hendrerit dolor magna eget est lorem ipsum dolor sit amet.</p>
+					
+					<div class="testimonial_meta">
+						
+						<span>Client Name</span>
+						<span>Type of Case</span>
+						
+					</div><!-- testimonial_meta -->
+				
+				</div><!-- single_testimonial_inner -->
+				
+			</div><!-- single_testimonial -->
+			
+		</div><!-- testimonial_wrapper -->
+			
+	</div><!-- outer_container -->
+	
+</div><!-- internal_main -->
+
 
 
 <?php get_footer(); ?>
