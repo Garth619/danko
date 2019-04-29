e0b905e5632ae2ce2c570ea9b765de9c51f85ded
Garrett Cullen <garrettwcullen@gmail.com>
2019-04-29 15:01:22 -0700

    push

--- wp-content/themes/danko/page-templates/template-bio.php
+++ wp-content/themes/danko/page-templates/template-bio.php
@@ -6,7 +6,144 @@ get_header(); ?>
 
 
 
-	<?php get_template_part( 'loop', 'page' ); ?>
+	<div id="internal_main">
+	
+
+	
+	<div id="outer_trigger" class="outer_container">
+		
+		<div class="d_m_slide dm_internal">
+		
+			<span>D</span>
+			<span>M</span>
+	
+		</div><!-- d_m_slide -->
+		
+		<div class="att_bio_container content">
+			
+			<div class="att_bio_title_wrapper">
+				
+				<img class="wreath" src="<?php bloginfo('template_directory');?>/images/ico-star-wreath.svg"/>
+				
+				<h1><?php the_title();?></h1>
+				
+				<span class="att_position">Founding Partner</span><!-- att_position -->
+				
+			</div><!-- att_bio_title_wrapper -->
+			
+			<div class="att_bio_inner">
+				
+				<div class="att_bio_left">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/att-placeholder.jpg"/>
+					
+				</div><!-- att_bio_left -->
+				
+				<div class="att_bio_right">
+					
+					<p>Mike Danko is the Danko Meredith’s founding partner. Although his office is located in the San Francisco Bay Area, he has tried cases from coast to coast, literally from Hawaii to Massachusetts.  The $15 million jury verdict he obtained for his client in Burdett v. Teledyne earned him a nomination for California Trial Lawyer of the Year by the Consumer Attorneys of California, the state’s trial lawyer association.  He has been named one of Northern California’s Top 100 “Super Lawyers.”</p>
+
+					<p>Mr. Danko has extensive experience representing those seriously injured in underground utility explosions and fires. He has represented victims of accidents involving cars, bicycles, aircraft of all different types, and various household products.  He represents those injured by dangerous drugs such as Yaz and Yasmin.</p>
+					
+				</div><!-- att_bio_right -->
+				
+			</div><!-- att_bio_inner -->
+			
+			<div class="att_bio_bottom">
+				
+				<blockquote>By holding wrongdoers accountable, a lawsuit can take the profit out of harming others. It can deter wrongdoing in the future and change for the better the way corporations do business.</blockquote>
+				
+				<p>Mr. Danko is a frequent speaker on legal topics. He has been invited to appear on national television shows such as The CBS Early Show, Good Morning America, Fox News, and CBS News. His work has been written about in such print media as the Wall Street Journal, USA Today, Forbes, the Boston Globe, the Los Angeles Times, the San Francisco Chronicle and the Houston Chronicle.</p>
+
+				<h2>Biography </h2>
+				
+				<p>Mike Danko graduated from Dartmouth College, magna cum laude, and then earned his law degree at the University of Virginia School of Law.  He began his legal career working for an internationally known Wall Street law firm, representing the nation’s leading financial institutions in high-stakes business litigation.</p>
+
+				<p>Finding this aspect of the law to be more lucrative than it was fulfilling, Mike turned to representing people who have suffered catastrophic injuries, and the families of those who have been killed, due to the negligence or wrongdoing of another party.  Over the past 15 years, he has earned millions of dollars in settlements, and in judge and jury awards for his clients that have helped pay for ongoing medical treatment and care for injured victims, and compensated families for the loss of a loved one.</p>
+
+				<p>Mike has been honored with a Trial Lawyer of the Year nomination from his state's trial lawyers association, the Consumer Attorneys of California. Mike has been named a Northern California Super Lawyer each year since the award’s inception in 2004, an honor accorded to less than 5 percent of the state’s lawyers.  Thomson Reuters named him one of the “Top 100” Super Lawyers in Northern California in 2012, 2013, 2014 and 2016. He has been named to Best Lawyers in America. He is a member of the American Association for Justice, the Lawyer Pilots Bar Association and the Consumer Attorneys of California.</p>
+
+				<p>As a spokesman for his clients and to prevent other injuries and deaths--from such causes as candy that chokes children, or old tires subject to sudden detreading--Mike has appeared on numerous local and national radio and television shows, and has been interviewed by USA Today, the New York Times, the Wall Street Journal, Forbes, Bloomberg News, and many other national and local media venues.</p>
+				
+				
+				<div class="att_accolades">
+					
+					<div class="att_accolades_col">
+						
+						<div class="single_accolade">
+							
+							<span class="accolade_title">Professional Associations</span><!-- accolade_title -->
+							
+							<ul>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+							</ul>
+							
+						</div><!-- single_accolade -->
+						
+						<div class="single_accolade">
+							
+							<span class="accolade_title">Professional Associations</span><!-- accolade_title -->
+							
+							<ul>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+							</ul>
+							
+						</div><!-- single_accolade -->
+						
+					</div><!-- att_accolades_col -->
+					
+						<div class="att_accolades_col">
+						
+						<div class="single_accolade">
+							
+							<span class="accolade_title">Professional Associations</span><!-- accolade_title -->
+							
+							<ul>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+							</ul>
+							
+						</div><!-- single_accolade -->
+						
+						<div class="single_accolade">
+							
+							<span class="accolade_title">Professional Associations</span><!-- accolade_title -->
+							
+							<ul>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+								<li>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing</li>
+							</ul>
+							
+						</div><!-- single_accolade -->
+						
+					</div><!-- att_accolades_col -->
+
+				</div><!-- att_accolades -->
+				
+			</div><!-- att_bio_bottom -->
+			
+		<?php //get_template_part( 'loop', 'page' ); ?>
+			
+		</div><!-- att_bio_containers -->
+		
+	</div><!-- outer_container -->
+	
+</div><!-- internal_main -->
+
 					 
 					 	
 <?php get_footer(); ?>
